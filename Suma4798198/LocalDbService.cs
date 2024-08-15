3844d093ecc317fa213efc2170b9e546288821ec
Esme <Esme@DESKTOP-7NA17H9>
2024-08-15 14:41:18 -0600

    Agregar archivos de proyecto.

---
+++ Suma4798198/LocalDbService.cs
@@ -0,0 +1,44 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma4798198
+{
+    public class LocalDbService
+    {
+        private const string DB_NAME = "demo_suma.db3";
+        private readonly SQLiteAsyncConnection _connection;
+
+        public LocalDbService()
+        {
+            _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
+            //Le indica al sistema qe cree la tabla de nuestro contexto
+            _connection.CreateTableAsync<Resultado>();
+        }
+        //Método para listar los registros de nuestra tabla
+
+        public async Task<List<Resultado>> GetResultado()
+        {
+            return await _connection.Table<Resultado>().ToListAsync();
+        }
+
+        //Método para crear registros 
+        public async Task Create(Resultado resultado)
+        {
+            await _connection.InsertAsync(resultado);
+        }
+        //Método para actualizar
+        public async Task Update(Resultado resultado)
+        {
+            await  _connection.UpdateAsync(resultado);
+        }
+        //Método para eliminar
+        public async Task Delete(Resultado resultado)
+        {
+            await _connection.DeleteAsync(resultado);
+        }
+    }
+}
---
+++ Suma4798198/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Suma4798198
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
